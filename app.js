0fd43e0d881a19bd7d7088508f969fb1b49cb511
Saksham <bhardwajsaksham796@gmail.com>
2024-02-14 00:05:41 +0530

    video 1

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+const PORT= process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send("hi, i am live")
+})
+const start=async ()=>{
+ try {
+    app.listen(PORT,()=>{
+        console.log("Database Connected")
+    });
+ }
+ catch(error){
+    console.log(error)
+ }
+};
+start();
