3d486bc1fe8e89b25c85884a2c9872492669b089
Saksham <bhardwajsaksham796@gmail.com>
2024-02-14 03:26:23 +0530

    un video

--- controllers/products.js
+++ controllers/products.js
@@ -3,21 +3,37 @@ const products = require("../models/products");
 
 const getAllProducts=async (req,res)=>{
     // console.log(req.query);
-    const {company,name}=req.query;
+    const {company,name,featured}=req.query;
     const queryObj={};
+    
     if(company){
-        queryObj.company=company
-        console.log(queryObj);
+        queryObj.company=company;
+    
+    }
+    if(featured){
+        queryObj.featured=featured;
+    }
+    if(name){
+        queryObj.name={ $regex: name, $options: "i" };
+       
     }
     const producter=await products.find(queryObj);
     res.status(200).json({producter});
 };
 const getAllProductsTesting=async (req,res)=>{
-    const {company}=req.query;
+   const {company,name,featured}=req.query;
     const queryObj={};
+    
     if(company){
-        queryObj.company=company
-        console.log(queryObj);
+        queryObj.company=company;
+    
+    }
+    if(featured){
+        queryObj.featured=featured;
+    }
+    if(name){
+        queryObj.name={$regex : name,$options : "i" };
+       
     }
     const producter=await products.find(queryObj);
     res.status(200).json({producter});
