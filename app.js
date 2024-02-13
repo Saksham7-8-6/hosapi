cd19df22cfb428a74088a4705baf0cbd89b8c962
Saksham <bhardwajsaksham796@gmail.com>
2024-02-14 00:24:04 +0530

    second video

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express=require('express');
 const app=express();
+
 const PORT= process.env.PORT || 5000;
+
+const products_routes=require('./routes/products')
+
 app.get('/',(req,res)=>{
     res.send("hi, i am live")
-})
+});
+
+// middleware or to set router
+
+app.use("/api/products",products_routes);
 const start=async ()=>{
  try {
     app.listen(PORT,()=>{
