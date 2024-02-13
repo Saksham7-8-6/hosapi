26a4a77d3f1dedb13d541febd6fec0144bfc2763
Saksham <bhardwajsaksham796@gmail.com>
2024-02-14 02:14:20 +0530

    eight video

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,11 @@
+const products = require("../models/products");
 
 const getAllProducts=async (req,res)=>{
-    res.status(200).json({msg:"I am getAllProducts"})
+    const producter=await products.find({});
+    res.status(200).json({producter});
 };
 const getAllProductsTesting=async (req,res)=>{
-    res.status(200).json({msg:"I am getAllProductsTesting"})
+    res.status(200).json({producter});
 };
 
 module.exports={getAllProducts,getAllProductsTesting}
