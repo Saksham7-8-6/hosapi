60360e6a9fcf1a46a8cc4dc743c09c7d6b8696b1
Saksham <bhardwajsaksham796@gmail.com>
2024-02-14 02:21:38 +0530

    eight video

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const products = require("../models/products");
 
 const getAllProducts=async (req,res)=>{
-    const producter=await products.find({});
+    const producter=await products.find(req.query);
     res.status(200).json({producter});
 };
 const getAllProductsTesting=async (req,res)=>{
