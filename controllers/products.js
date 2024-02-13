cb443162a198ea76542c2bbf7ad55542d3fb5769
Saksham <bhardwajsaksham796@gmail.com>
2024-02-14 03:00:38 +0530

    eight video

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,25 @@
 const products = require("../models/products");
 
+
 const getAllProducts=async (req,res)=>{
-    const producter=await products.find(req.query);
+    // console.log(req.query);
+    const {company,name}=req.query;
+    const queryObj={};
+    if(company){
+        queryObj.company=company
+        console.log(queryObj);
+    }
+    const producter=await products.find(queryObj);
     res.status(200).json({producter});
 };
 const getAllProductsTesting=async (req,res)=>{
-    const producter=await products.find(req.query);
+    const {company}=req.query;
+    const queryObj={};
+    if(company){
+        queryObj.company=company
+        console.log(queryObj);
+    }
+    const producter=await products.find(queryObj);
     res.status(200).json({producter});
 };
 
--- productDB.js
+++ productDB.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const connectDB=require("./db/connect");
+const products = require("./models/products");
 const Product=require("./models/products");
 
 const ProductsJson=require("./product.json");
@@ -7,10 +8,10 @@ const ProductsJson=require("./product.json");
 const start=async()=>{
     try{
         await connectDB(process.env.MONGODB_URI);
+        await products.deleteMany();  // delete the prvious data after again calling it 
         await Product.create(ProductsJson);
         console.log("Success")
-    }
-    catch(error){
+    }catch(error){
         console.log(error);
     }
 };
