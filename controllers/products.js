4637b39fe9dc565ab23ed88d3813e72267919467
Saksham <bhardwajsaksham796@gmail.com>
2024-02-14 13:18:58 +0530

    last

--- controllers/products.js
+++ controllers/products.js
@@ -21,16 +21,21 @@ const getAllProducts=async (req,res)=>{
     if(sort){
         let sortfix=sort.replace(","," ");
         apidata=apidata.sort(sortfix);
-
+        
     }
     if(select){
         let sortfix=select.split(",").join(" ");
         apidata=apidata.select(sortfix);
-
+        
     }
+    let page=Number(req.query.page)||2;
+    let limit=Number(req.query.limit)||1;
+    let skip=(page-1)*limit;
+    
+    apidata=apidata.skip(skip).limit(limit);
     console.log(queryObj);
-    const producter=await apidata;
-    res.status(200).json({producter});
+    const products=await apidata;
+    res.status(200).json({products,nbHits:products.length});
 };
 const getAllProductsTesting=async (req,res)=>{
    const {company,name,featured}=req.query;
